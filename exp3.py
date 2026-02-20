0d4c86023c67486a2152df2e8fde8a4b791d66f4
kritisocial806-ctrl <kritisocial806@gmail.com>
2026-02-21 00:46:46 +0530

    Add files via upload

---
+++ exp3.py
@@ -0,0 +1,10 @@
+def kthSmallest(arr, k):
+    arr.sort()
+    return arr[k-1]
+
+# Example
+arr = [10, 5, 4, 3, 48, 6, 2, 33, 53, 10]
+k = 4
+
+result = kthSmallest(arr, k)
+print("Kth Smallest Element:", result)
